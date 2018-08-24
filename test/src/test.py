5b7e620df944ab54413634b2dd496c9f9006bf9f
Herminio Vazquez <canimus@gmail.com>
2018-08-24 02:41:43 +0200

    Addition of build compose distributed

---
+++ test/src/test.py
@@ -0,0 +1,16 @@
+import pytest
+
+def test_initial_report():
+    assert True
+
+def test_setup():
+    assert True
+
+def test_tear_down():
+    assert True
+
+def test_move():
+    assert True
+
+def test_run2():
+    assert True
